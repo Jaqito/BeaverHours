1a55c1db035d97ef083aa78b48f4c6771fe34a32
Jack Donkers <jackdonkers@Jacks-MacBook-Pro-2.local>
2022-01-21 15:11:31 -0800

    update .env to only run on local

--- bot/loaders/cosmosDbStorage.ts
+++ bot/loaders/cosmosDbStorage.ts
@@ -1,7 +1,10 @@
 import * as path from 'path';
-const ENV_FILE = path.join(__dirname, '.env.teamsfx.local');
 import { CosmosDbPartitionedStorage } from 'botbuilder-azure';
-require('dotenv').config({ path: ENV_FILE });
+const dotenv = require('dotenv');
+if (process.env.NODE_ENV !== 'production') {
+    const ENV_FILE = path.join(__dirname, '.env.teamsfx.local');
+    dotenv.config({ path: ENV_FILE });
+}
 
 const cosmosDbStorage = new CosmosDbPartitionedStorage({
     cosmosDbEndpoint: process.env.CosmosDbEndpoint,
