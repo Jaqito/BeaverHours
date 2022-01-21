13a2e0cb2d1092697e998f75968f1b255739279e
Jack Donkers <jackdonkers@Jacks-MacBook-Pro-2.local>
2022-01-21 15:16:01 -0800

    run prettier

--- bot/loaders/cosmosDbStorage.ts
+++ bot/loaders/cosmosDbStorage.ts
@@ -1,17 +1,17 @@
-import * as path from 'path';
-import { CosmosDbPartitionedStorage } from 'botbuilder-azure';
-const dotenv = require('dotenv');
-if (process.env.NODE_ENV !== 'production') {
-    const ENV_FILE = path.join(__dirname, '.env.teamsfx.local');
-    dotenv.config({ path: ENV_FILE });
+import * as path from "path";
+import { CosmosDbPartitionedStorage } from "botbuilder-azure";
+const dotenv = require("dotenv");
+if (process.env.NODE_ENV !== "production") {
+  const ENV_FILE = path.join(__dirname, ".env.teamsfx.local");
+  dotenv.config({ path: ENV_FILE });
 }
 
 const cosmosDbStorage = new CosmosDbPartitionedStorage({
-    cosmosDbEndpoint: process.env.CosmosDbEndpoint,
-    authKey: process.env.CosmosDbAuthKey,
-    databaseId: process.env.CosmosDbDatabaseId,
-    containerId: process.env.CosmosDbContainerId,
-    compatibilityMode: false,
+  cosmosDbEndpoint: process.env.CosmosDbEndpoint,
+  authKey: process.env.CosmosDbAuthKey,
+  databaseId: process.env.CosmosDbDatabaseId,
+  containerId: process.env.CosmosDbContainerId,
+  compatibilityMode: false,
 });
 
 export default cosmosDbStorage;
