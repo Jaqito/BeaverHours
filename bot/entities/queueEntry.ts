335ae353e1e68be376c9ba8d6d2fdbf494e2d166
Jack Donkers <jackdonkers@Jacks-MacBook-Pro-2.local>
2022-02-01 23:31:09 -0800

    prettier

--- bot/entities/queueEntry.ts
+++ bot/entities/queueEntry.ts
@@ -1,36 +1,36 @@
 import {
-    BaseEntity,
-    Column,
-    CreateDateColumn,
-    Entity,
-    JoinColumn,
-    ManyToOne,
-    PrimaryGeneratedColumn,
-    UpdateDateColumn,
-} from 'typeorm';
-import { Queue } from './queue';
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { Queue } from "./queue";
 
-@Entity('queueEntry')
+@Entity("queueEntry")
 export class QueueEntry extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    id!: number;
+  @PrimaryGeneratedColumn()
+  id!: number;
 
-    @Column()
-    userId: string;
+  @Column()
+  userId: string;
 
-    @Column()
-    question?: string;
+  @Column()
+  question?: string;
 
-    @Column()
-    resolved: boolean;
+  @Column()
+  resolved: boolean;
 
-    @CreateDateColumn()
-    createdAt: Date = new Date();
+  @CreateDateColumn()
+  createdAt: Date = new Date();
 
-    @UpdateDateColumn()
-    updatedAt: Date = new Date();
+  @UpdateDateColumn()
+  updatedAt: Date = new Date();
 
-    @ManyToOne(() => Queue, (queue) => queue.queueEntries)
-    @JoinColumn({ name: 'queueId' })
-    queue: Queue;
+  @ManyToOne(() => Queue, (queue) => queue.queueEntries)
+  @JoinColumn({ name: "queueId" })
+  queue: Queue;
 }
