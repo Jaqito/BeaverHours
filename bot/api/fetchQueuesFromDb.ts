835683aac63ec563e87bc1c96876c19fa9a276e2
Jack Donkers <jackdonkers@Jacks-MacBook-Pro-2.local>
2022-02-03 10:30:07 -0800

    fetch queues based off status

---
+++ bot/api/fetchQueuesFromDb.ts
@@ -0,0 +1,18 @@
+import { Connection } from 'typeorm';
+import Queue from '../utilities/Queue';
+import { Queue as QueueEntity } from '../entities/queue';
+import { QueueStatus } from '../utilities/Global';
+
+export default async (conn: Connection, queueStatus: QueueStatus, channelId: string) => {
+    try {
+        const result = await conn
+            .getRepository(QueueEntity)
+            .createQueryBuilder()
+            .where(`status = :status AND "channelId" = :channelId`, { status: queueStatus, channelId: channelId })
+            .getMany();
+        return result;
+    } catch (e) {
+        console.log(`Failed to fetch Queues to Database with status ${queueStatus} and channelId: ${channelId}`, e);
+        throw e;
+    }
+};
