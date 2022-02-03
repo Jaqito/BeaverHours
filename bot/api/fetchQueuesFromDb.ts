48eecd85016aac71934d2fc58f208184f195d65f
Jack Donkers <jackdonkers@Jacks-MacBook-Pro-2.local>
2022-02-03 10:36:01 -0800

    prettier

--- bot/api/fetchQueuesFromDb.ts
+++ bot/api/fetchQueuesFromDb.ts
@@ -1,18 +1,28 @@
-import { Connection } from 'typeorm';
-import Queue from '../utilities/Queue';
-import { Queue as QueueEntity } from '../entities/queue';
-import { QueueStatus } from '../utilities/Global';
+import { Connection } from "typeorm";
+import Queue from "../utilities/Queue";
+import { Queue as QueueEntity } from "../entities/queue";
+import { QueueStatus } from "../utilities/Global";
 
-export default async (conn: Connection, queueStatus: QueueStatus, channelId: string) => {
-    try {
-        const result = await conn
-            .getRepository(QueueEntity)
-            .createQueryBuilder()
-            .where(`status = :status AND "channelId" = :channelId`, { status: queueStatus, channelId: channelId })
-            .getMany();
-        return result;
-    } catch (e) {
-        console.log(`Failed to fetch Queues to Database with status ${queueStatus} and channelId: ${channelId}`, e);
-        throw e;
-    }
+export default async (
+  conn: Connection,
+  queueStatus: QueueStatus,
+  channelId: string
+) => {
+  try {
+    const result = await conn
+      .getRepository(QueueEntity)
+      .createQueryBuilder()
+      .where(`status = :status AND "channelId" = :channelId`, {
+        status: queueStatus,
+        channelId: channelId,
+      })
+      .getMany();
+    return result;
+  } catch (e) {
+    console.log(
+      `Failed to fetch Queues to Database with status ${queueStatus} and channelId: ${channelId}`,
+      e
+    );
+    throw e;
+  }
 };
