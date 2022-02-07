3088df69c000b90cf0d24e0e9a6bda9bff866b3f
Rohit Chaudhary <40576340+rorochaudhary@users.noreply.github.com>
2022-02-06 17:27:00 -0800

    update state handling via use of context, working

--- tabs/src/components/BeaverHours/SchedulerRewrite.tsx
+++ tabs/src/components/BeaverHours/SchedulerRewrite.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import {
-  createContext,
-  useContext,
-  // useRef,
-  useState,
-} from "react";
+import { createContext, Dispatch, useContext, useState } from "react";
 
 const ScheduleContext = createContext({
-  state: {},
-  setSchedule: (scheduleUpdate: any) => {},
+  state: {} as OfficeHoursSchedule,
+  updateSchedule: (newSchedule: OfficeHoursSchedule) => {},
 });
 
 export function Scheduler() {
@@ -33,7 +28,7 @@ interface OfficeHoursSchedule {
 }
 
 export function ScheduleForm() {
-  const [state, setState] = useState({
+  const [state, setSchedule] = useState({
     Sunday: {} as DaySchedule,
     Monday: {} as DaySchedule,
     Tuesday: {} as DaySchedule,
@@ -43,14 +38,18 @@ export function ScheduleForm() {
     Saturday: {} as DaySchedule,
   } as OfficeHoursSchedule);
 
-  const updater = (scheduleUpdate: any) => {
-    setState({ ...state, scheduleUpdate });
+  const updateSchedule = (newSchedule: OfficeHoursSchedule) => {
+    setSchedule(newSchedule);
+  };
+  const value = { state, updateSchedule };
+
+  const HandleSubmit = (event: any) => {
+    console.log("A schedule was submitted:\n", state);
+    event.preventDefault();
   };
 
-  const scheduleUpdater = { state: state, setSchedule: updater };
-
   return (
-    <ScheduleContext.Provider value={scheduleUpdater}>
+    <ScheduleContext.Provider value={value}>
       <form onSubmit={HandleSubmit}>
         <GenerateWeekTable />
         <input type="submit" value="submit" />
@@ -84,6 +83,71 @@ export function GenerateWeekTable() {
 }
 
 export function NewRow(name: string) {
+  const scheduler = useContext(ScheduleContext);
+
+  const HandleChange = (event: any) => {
+    event.persist();
+    var splitname = event.target.name.split("-");
+    var dayOfWeek = splitname[0];
+    var fieldname = splitname[1];
+    var newSchedule = {};
+    if (fieldname === "startAt") {
+      console.log("edited startAt for" + dayOfWeek);
+      //   newSchedule = {
+      //     [dayOfWeek]: {
+      //       startAt: event.target.value,
+      //       endAt: scheduler.state[dayOfWeek]?.endAt,
+      //       repeated: scheduler.state[dayOfWeek]?.repeated,
+      //     } as DaySchedule,
+      //   };
+      //   scheduler.updateSchedule({ ...scheduler.state, newSchedule });
+      scheduler.updateSchedule({
+        ...scheduler.state,
+        [dayOfWeek]: {
+          startAt: event.target.value,
+          endAt: scheduler.state[dayOfWeek]?.endAt,
+          repeated: scheduler.state[dayOfWeek]?.repeated,
+        } as DaySchedule,
+      });
+    } else if (fieldname === "endAt") {
+      console.log("edited endAt for" + dayOfWeek);
+      //   newSchedule = {
+      //     [dayOfWeek]: {
+      //       startAt: scheduler.state[dayOfWeek]?.startAt,
+      //       endAt: event.target.value,
+      //       repeated: scheduler.state[dayOfWeek]?.repeated,
+      //     } as DaySchedule,
+      //   };
+      //   scheduler.updateSchedule({ ...scheduler.state, newSchedule });
+      scheduler.updateSchedule({
+        ...scheduler.state,
+        [dayOfWeek]: {
+          startAt: scheduler.state[dayOfWeek]?.startAt,
+          endAt: event.target.value,
+          repeated: scheduler.state[dayOfWeek]?.repeated,
+        } as DaySchedule,
+      });
+    } else if (fieldname === "repeated") {
+      console.log("edited repeated for" + dayOfWeek);
+      //   newSchedule = {
+      // [dayOfWeek]: {
+      //   startAt: scheduler.state[dayOfWeek]?.startAt,
+      //   endAt: scheduler.state[dayOfWeek]?.endAt,
+      //   repeated: event.target.value,
+      // } as DaySchedule,
+      //   };
+      //   scheduler.updateSchedule({ ...scheduler.state, newSchedule });
+      scheduler.updateSchedule({
+        ...scheduler.state,
+        [dayOfWeek]: {
+          startAt: scheduler.state[dayOfWeek]?.startAt,
+          endAt: scheduler.state[dayOfWeek]?.endAt,
+          repeated: event.target.value,
+        } as DaySchedule,
+      });
+    }
+  };
+
   return (
     <tr>
       <th>{name}</th>
@@ -119,60 +183,49 @@ export function AddRow(name: string) {
   return NewRow(name);
 }
 
-export function HandleChange(event: any) {
-  event.persist();
-  const scheduleUpdater = useContext(ScheduleContext);
+// export function HandleChange(event: any) {
+//   event.persist();
+//   const scheduler = useContext(ScheduleContext);
 
-  var splitname = event.target.name.split("-");
-  var dayOfWeek = splitname[0];
-  var fieldname = splitname[1];
-  if (fieldname === "startAt") {
-    console.log("edited startAt for" + dayOfWeek);
-    scheduleUpdater.setSchedule({
-      startAt: event.target.value,
-    });
-    // this.setState((state) => {
-    //     return {
-    //         [dayOfWeek]: {
-    //         startAt: event.target.value,
-    //         endAt: state[dayOfWeek]?.endAt,
-    //         repeated: state[dayOfWeek]?.repeated,
-    //         },
-    //     };
-    // });
-  } else if (fieldname === "endAt") {
-    console.log("edited endAt for" + dayOfWeek);
-    scheduleUpdater.setSchedule({
-      endAt: event.target.value,
-    });
-    // this.setState((state) => {
-    //     return {
-    //         [dayOfWeek]: {
-    //         startAt: state[dayOfWeek]?.startAt,
-    //         endAt: event.target.value,
-    //         repeated: state[dayOfWeek]?.repeated,
-    //         },
-    //     };
-    // });
-  } else if (fieldname === "repeated") {
-    console.log("edited repeated for" + dayOfWeek);
-    scheduleUpdater.setSchedule({
-      repeated: event.target.value,
-    });
-    // this.setState((state) => {
-    //     return {
-    //         [dayOfWeek]: {
-    //         startAt: state[dayOfWeek]?.startAt,
-    //         endAt: state[dayOfWeek]?.endAt,
-    //         repeated: event.target.value,
-    //         },
-    //     };
-    // });
-  }
-}
+//   var splitname = event.target.name.split("-");
+//   var dayOfWeek = splitname[0];
+//   var fieldname = splitname[1];
+//   var newSchedule = {};
+//   if (fieldname === "startAt") {
+//     console.log("edited startAt for" + dayOfWeek);
+//     newSchedule = {
+//         [dayOfWeek]: {
+//             startAt: event.target.value,
+//             endAt: scheduler.state[dayOfWeek]?.endAt,
+//             repeated: scheduler.state[dayOfWeek]?.repeated,
+//         } as DaySchedule
+//     };
+//     scheduler.updateSchedule({...scheduler.state, newSchedule})
+//   } else if (fieldname === "endAt") {
+//     console.log("edited endAt for" + dayOfWeek);
+//     newSchedule = {
+//         [dayOfWeek]: {
+//             startAt: scheduler.state[dayOfWeek]?.startAt,
+//             endAt: event.target.value,
+//             repeated: scheduler.state[dayOfWeek]?.repeated,
+//         } as DaySchedule
+//     };
+//     scheduler.updateSchedule({...scheduler.state, newSchedule})
+//   } else if (fieldname === "repeated") {
+//     console.log("edited repeated for" + dayOfWeek);
+//     newSchedule = {
+//         [dayOfWeek]: {
+//             startAt: scheduler.state[dayOfWeek]?.startAt,
+//             endAt: scheduler.state[dayOfWeek]?.endAt,
+//             repeated: event.target.value,
+//         } as DaySchedule
+//     };
+//     scheduler.updateSchedule({...scheduler.state, newSchedule})
+//   }
+// }
 
-export function HandleSubmit(event: any) {
-  const scheduleUpdater = useContext(ScheduleContext);
-  console.log("A schedule was submitted:\n", scheduleUpdater.state);
-  event.preventDefault();
-}
+// export function HandleSubmit(event: any) {
+//   const scheduleUpdater = useContext(ScheduleContext);
+//   console.log("A schedule was submitted:\n", scheduleUpdater.state);
+//   event.preventDefault();
+// }
