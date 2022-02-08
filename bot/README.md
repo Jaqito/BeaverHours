2b36985d988145b7979d3762118bd35b74045a0e
Jack Donkers <jackdonkers@Jacks-MacBook-Pro-2.local>
2022-02-07 23:36:05 -0800

    prettier

--- bot/README.md
+++ bot/README.md
@@ -10,22 +10,22 @@ This is a simple hello world application with both Bot and Messaging extension c
 
 ## Prerequisites
 
--   [NodeJS](https://nodejs.org/en/)
--   An M365 account. If you do not have M365 account, apply one from [M365 developer program](https://developer.microsoft.com/en-us/microsoft-365/dev-program)
--   [Teams Toolkit Visual Studio Code Extension](https://aka.ms/teams-toolkit) version after 1.55 or [TeamsFx CLI](https://aka.ms/teamsfx-cli)
+- [NodeJS](https://nodejs.org/en/)
+- An M365 account. If you do not have M365 account, apply one from [M365 developer program](https://developer.microsoft.com/en-us/microsoft-365/dev-program)
+- [Teams Toolkit Visual Studio Code Extension](https://aka.ms/teams-toolkit) version after 1.55 or [TeamsFx CLI](https://aka.ms/teamsfx-cli)
 
 ## Debug
 
--   From Visual Studio Code: Start debugging the project by hitting the `F5` key in Visual Studio Code.
--   Alternatively use the `Run and Debug Activity Panel` in Visual Studio Code and click the `Run and Debug` green arrow button.
--   From TeamsFx CLI: Start debugging the project by executing the command `teamsfx preview --local` in your project directory.
+- From Visual Studio Code: Start debugging the project by hitting the `F5` key in Visual Studio Code.
+- Alternatively use the `Run and Debug Activity Panel` in Visual Studio Code and click the `Run and Debug` green arrow button.
+- From TeamsFx CLI: Start debugging the project by executing the command `teamsfx preview --local` in your project directory.
 
 ## Edit the manifest
 
 You can find the Teams app manifest in `templates/appPackage` folder. The folder contains two manifest files:
 
--   `manifest.local.template.json`: Manifest file for Teams app running locally.
--   `manifest.remote.template.json`: Manifest file for Teams app running remotely (After deployed to Azure).
+- `manifest.local.template.json`: Manifest file for Teams app running locally.
+- `manifest.remote.template.json`: Manifest file for Teams app running remotely (After deployed to Azure).
 
 Both files contain template arguments with `{...}` statements which will be replaced at build time. You may add any extra properties or permissions you require to this file. See the [schema reference](https://docs.microsoft.com/en-us/microsoftteams/platform/resources/schema/manifest-schema) for more information.
 
@@ -43,41 +43,41 @@ Deploy your project to Azure by following these steps:
 
 Once the provisioning and deployment steps are finished, you can preview your app:
 
--   From Visual Studio Code
+- From Visual Studio Code
 
-    1. Open the `Run and Debug Activity Panel`.
-    1. Select `Launch Remote (Edge)` or `Launch Remote (Chrome)` from the launch configuration drop-down.
-    1. Press the Play (green arrow) button to launch your app - now running remotely from Azure.
+  1. Open the `Run and Debug Activity Panel`.
+  1. Select `Launch Remote (Edge)` or `Launch Remote (Chrome)` from the launch configuration drop-down.
+  1. Press the Play (green arrow) button to launch your app - now running remotely from Azure.
 
--   From TeamsFx CLI: execute `teamsfx preview --remote` in your project directory to launch your application.
+- From TeamsFx CLI: execute `teamsfx preview --remote` in your project directory to launch your application.
 
 ## Validate manifest file
 
 To check that your manifest file is valid:
 
--   From Visual Studio Code: open the command palette and select: `Teams: Validate manifest file`.
--   From TeamsFx CLI: run command `teamsfx validate` in your project directory.
+- From Visual Studio Code: open the command palette and select: `Teams: Validate manifest file`.
+- From TeamsFx CLI: run command `teamsfx validate` in your project directory.
 
 ## Package
 
--   From Visual Studio Code: open the Teams Toolkit and click `Zip Teams metadata package` or open the command palette and select `Teams: Zip Teams metadata package`.
--   Alternatively, from the command line run `teamsfx package` in the project directory.
+- From Visual Studio Code: open the Teams Toolkit and click `Zip Teams metadata package` or open the command palette and select `Teams: Zip Teams metadata package`.
+- Alternatively, from the command line run `teamsfx package` in the project directory.
 
 ## Publish to Teams
 
 Once deployed, you may want to distribute your application to your organization's internal app store in Teams. Your app will be submitted for admin approval.
 
--   From Visual Studio Code: open the Teams Toolkit and click `Publish to Teams` or open the command palette and select: `Teams: Publish to Teams`.
--   From TeamsFx CLI: run command `teamsfx publish` in your project directory.
+- From Visual Studio Code: open the Teams Toolkit and click `Publish to Teams` or open the command palette and select: `Teams: Publish to Teams`.
+- From TeamsFx CLI: run command `teamsfx publish` in your project directory.
 
 ### Bot
 
--   [Bot Basics](https://docs.microsoft.com/azure/bot-service/bot-builder-basics?view=azure-bot-service-4.0)
--   [Bot Framework Documentation](https://docs.botframework.com/)
--   [Azure Bot Service Introduction](https://docs.microsoft.com/azure/bot-service/bot-service-overview-introduction?view=azure-bot-service-4.0)
+- [Bot Basics](https://docs.microsoft.com/azure/bot-service/bot-builder-basics?view=azure-bot-service-4.0)
+- [Bot Framework Documentation](https://docs.botframework.com/)
+- [Azure Bot Service Introduction](https://docs.microsoft.com/azure/bot-service/bot-service-overview-introduction?view=azure-bot-service-4.0)
 
 ### Messaging Extension
 
--   [Search Command](https://docs.microsoft.com/en-us/microsoftteams/platform/messaging-extensions/how-to/search-commands/define-search-command)
--   [Action Command](https://docs.microsoft.com/en-us/microsoftteams/platform/messaging-extensions/how-to/action-commands/define-action-command)
--   [Link Unfurling](https://docs.microsoft.com/en-us/microsoftteams/platform/messaging-extensions/how-to/link-unfurling?tabs=dotnet)
+- [Search Command](https://docs.microsoft.com/en-us/microsoftteams/platform/messaging-extensions/how-to/search-commands/define-search-command)
+- [Action Command](https://docs.microsoft.com/en-us/microsoftteams/platform/messaging-extensions/how-to/action-commands/define-action-command)
+- [Link Unfurling](https://docs.microsoft.com/en-us/microsoftteams/platform/messaging-extensions/how-to/link-unfurling?tabs=dotnet)
--- bot/teamsBot.ts
+++ bot/teamsBot.ts
@@ -1,170 +1,190 @@
-import { default as axios } from 'axios';
-import { CardFactory, TeamsActivityHandler, TurnContext, MessageFactory } from 'botbuilder';
-import * as querystring from 'querystring';
-import { Connection } from 'typeorm';
-import addQueueEntryToDb from './api/addQueueEntryToDb';
-import addQueueToDb from './api/addQueueToDb';
-import Queue from './utilities/Queue';
+import { default as axios } from "axios";
+import {
+  CardFactory,
+  TeamsActivityHandler,
+  TurnContext,
+  MessageFactory,
+} from "botbuilder";
+import * as querystring from "querystring";
+import { Connection } from "typeorm";
+import addQueueEntryToDb from "./api/addQueueEntryToDb";
+import addQueueToDb from "./api/addQueueToDb";
+import Queue from "./utilities/Queue";
 
 export interface DataInterface {
-    likeCount: number;
+  likeCount: number;
 }
 
 export class TeamsBot extends TeamsActivityHandler {
-    // record the likeCount
-    likeCountObj: { likeCount: number };
-    activeQueue: Queue;
-    dbConnection: Connection;
+  // record the likeCount
+  likeCountObj: { likeCount: number };
+  activeQueue: Queue;
+  dbConnection: Connection;
 
-    constructor(dbConnection: Connection) {
-        super();
-        this.dbConnection = dbConnection;
-        this.likeCountObj = { likeCount: 0 };
-        this.activeQueue = null;
+  constructor(dbConnection: Connection) {
+    super();
+    this.dbConnection = dbConnection;
+    this.likeCountObj = { likeCount: 0 };
+    this.activeQueue = null;
 
-        this.onMessage(async (context, next) => {
-            console.log('Running with Message Activity.');
+    this.onMessage(async (context, next) => {
+      console.log("Running with Message Activity.");
 
-            let txt = context.activity.text;
-            const removedMentionText = TurnContext.removeRecipientMention(context.activity);
-            if (removedMentionText) {
-                // Remove the line break
-                txt = removedMentionText.toLowerCase().replace(/\n|\r/g, '').trim();
+      let txt = context.activity.text;
+      const removedMentionText = TurnContext.removeRecipientMention(
+        context.activity
+      );
+      if (removedMentionText) {
+        // Remove the line break
+        txt = removedMentionText.toLowerCase().replace(/\n|\r/g, "").trim();
+      }
+
+      // build a reusable mention to user that invoked bot
+      const mention = {
+        mentioned: context.activity.from,
+        text: `<at>${new TextEncoder().encode(
+          context.activity.from.name
+        )}</at>`,
+        type: "mention",
+      };
+
+      // Trigger command by IM text
+      switch (txt) {
+        case "start office hour": {
+          if (this.activeQueue) {
+            await context.sendActivity(
+              'Office hour already in progress. End active office hour with the command "end office hour"'
+            );
+          } else {
+            this.activeQueue = new Queue({
+              ownerId: context.activity.from.id,
+              channelId: context.activity.channelId,
+            });
+            const queue = await addQueueToDb(
+              this.dbConnection,
+              this.activeQueue
+            );
+            this.activeQueue.updateId(queue.id);
+            await context.sendActivity(
+              "<b>Started new Queue<b>\n\n" +
+                `<b>id</b>        ${this.activeQueue.properties.id}\n\n` +
+                `<b>ownerId</b>   ${this.activeQueue.properties.ownerId}\n\n` +
+                `<b>channelId</b> ${this.activeQueue.properties.channelId}\n\n` +
+                `<b>status</b>    ${this.activeQueue.properties.status}\n\n` +
+                `<b>at</b>        ${this.activeQueue.properties.startTime}`
+            );
+          }
+          break;
+        }
+        case "end office hour": {
+          if (this.activeQueue) {
+            this.activeQueue = null;
+            await context.sendActivity("Office hour successfully ended.");
+          } else {
+            await context.sendActivity(
+              'No office hour currently active. Start an office hour with the command "start office hour"'
+            );
+          }
+          break;
+        }
+        case "join office hours": {
+          if (this.activeQueue) {
+            if (this.activeQueue.checkQueue(context.activity.from.id)) {
+              const replyActivity = MessageFactory.text(
+                `Hello ${mention.text}! You are already in queue.`
+              );
+              replyActivity.entities = [mention];
+              await context.sendActivity(replyActivity);
+            } else {
+              this.activeQueue.enqueueStudent(context.activity.from.id);
+              await addQueueEntryToDb(
+                this.dbConnection,
+                context.activity.from.id,
+                this.activeQueue.properties.id
+              );
+              const replyActivity = MessageFactory.text(
+                `Hello ${
+                  mention.text
+                }! You have entered the office hours queue, the instructor will get to you! You are in position ${
+                  this.activeQueue.getQueuePosition(context.activity.from.id) +
+                  1
+                }.`
+              );
+              replyActivity.entities = [mention];
+              await context.sendActivity(replyActivity);
+              await context.sendActivity(
+                `Current queue: ${this.activeQueue.queueToString()}`
+              );
             }
-
-            // build a reusable mention to user that invoked bot
-            const mention = {
+          } else {
+            const replyActivity = MessageFactory.text(
+              `Hello ${mention.text}! Currently no office hours being held. Please check the schedule to confirm the next office hours session!`
+            );
+            replyActivity.entities = [mention];
+            await context.sendActivity(replyActivity);
+          }
+          break;
+        }
+        case "leave office hours": {
+          if (this.activeQueue) {
+            if (!this.activeQueue.checkQueue(context.activity.from.id)) {
+              const replyActivity = MessageFactory.text(
+                `Hello ${mention.text}! Unable to remove, you are currently not in a queue.`
+              );
+              replyActivity.entities = [mention];
+              await context.sendActivity(replyActivity);
+            } else {
+              this.activeQueue.dequeueStudent(context.activity.from.id);
+              const replyActivity = MessageFactory.text(
+                `Hello ${mention.text}! You have successfully been removed from the queue.`
+              );
+              replyActivity.entities = [mention];
+              await context.sendActivity(replyActivity);
+              await context.sendActivity(
+                `Current queue: ${this.activeQueue.queueToString()}`
+              );
+            }
+          } else {
+            const replyActivity = MessageFactory.text(
+              `Hello ${mention.text}! Currently no office hours being held. Please check the schedule to confirm the next office hours session!`
+            );
+            replyActivity.entities = [mention];
+            await context.sendActivity(replyActivity);
+          }
+          break;
+        }
+        case "get queue position": {
+          if (this.activeQueue) {
+            if (!this.activeQueue.checkQueue(context.activity.from.id)) {
+              await context.sendActivity("You are currently not in a queue.");
+            } else {
+              const mention = {
                 mentioned: context.activity.from,
-                text: `<at>${new TextEncoder().encode(context.activity.from.name)}</at>`,
-                type: 'mention',
-            };
-
-            // Trigger command by IM text
-            switch (txt) {
-                case 'start office hour': {
-                    if (this.activeQueue) {
-                        await context.sendActivity(
-                            'Office hour already in progress. End active office hour with the command "end office hour"'
-                        );
-                    } else {
-                        this.activeQueue = new Queue({
-                            ownerId: context.activity.from.id,
-                            channelId: context.activity.channelId,
-                        });
-                        const queue = await addQueueToDb(this.dbConnection, this.activeQueue);
-                        this.activeQueue.updateId(queue.id);
-                        await context.sendActivity(
-                            '<b>Started new Queue<b>\n\n' +
-                                `<b>id</b>        ${this.activeQueue.properties.id}\n\n` +
-                                `<b>ownerId</b>   ${this.activeQueue.properties.ownerId}\n\n` +
-                                `<b>channelId</b> ${this.activeQueue.properties.channelId}\n\n` +
-                                `<b>status</b>    ${this.activeQueue.properties.status}\n\n` +
-                                `<b>at</b>        ${this.activeQueue.properties.startTime}`
-                        );
-                    }
-                    break;
-                }
-                case 'end office hour': {
-                    if (this.activeQueue) {
-                        this.activeQueue = null;
-                        await context.sendActivity('Office hour successfully ended.');
-                    } else {
-                        await context.sendActivity(
-                            'No office hour currently active. Start an office hour with the command "start office hour"'
-                        );
-                    }
-                    break;
-                }
-                case 'join office hours': {
-                    if (this.activeQueue) {
-                        if (this.activeQueue.checkQueue(context.activity.from.id)) {
-                            const replyActivity = MessageFactory.text(
-                                `Hello ${mention.text}! You are already in queue.`
-                            );
-                            replyActivity.entities = [mention];
-                            await context.sendActivity(replyActivity);
-                        } else {
-                            this.activeQueue.enqueueStudent(context.activity.from.id);
-                            await addQueueEntryToDb(
-                                this.dbConnection,
-                                context.activity.from.id,
-                                this.activeQueue.properties.id
-                            );
-                            const replyActivity = MessageFactory.text(
-                                `Hello ${
-                                    mention.text
-                                }! You have entered the office hours queue, the instructor will get to you! You are in position ${
-                                    this.activeQueue.getQueuePosition(context.activity.from.id) + 1
-                                }.`
-                            );
-                            replyActivity.entities = [mention];
-                            await context.sendActivity(replyActivity);
-                            await context.sendActivity(`Current queue: ${this.activeQueue.queueToString()}`);
-                        }
-                    } else {
-                        const replyActivity = MessageFactory.text(
-                            `Hello ${mention.text}! Currently no office hours being held. Please check the schedule to confirm the next office hours session!`
-                        );
-                        replyActivity.entities = [mention];
-                        await context.sendActivity(replyActivity);
-                    }
-                    break;
-                }
-                case 'leave office hours': {
-                    if (this.activeQueue) {
-                        if (!this.activeQueue.checkQueue(context.activity.from.id)) {
-                            const replyActivity = MessageFactory.text(
-                                `Hello ${mention.text}! Unable to remove, you are currently not in a queue.`
-                            );
-                            replyActivity.entities = [mention];
-                            await context.sendActivity(replyActivity);
-                        } else {
-                            this.activeQueue.dequeueStudent(context.activity.from.id);
-                            const replyActivity = MessageFactory.text(
-                                `Hello ${mention.text}! You have successfully been removed from the queue.`
-                            );
-                            replyActivity.entities = [mention];
-                            await context.sendActivity(replyActivity);
-                            await context.sendActivity(`Current queue: ${this.activeQueue.queueToString()}`);
-                        }
-                    } else {
-                        const replyActivity = MessageFactory.text(
-                            `Hello ${mention.text}! Currently no office hours being held. Please check the schedule to confirm the next office hours session!`
-                        );
-                        replyActivity.entities = [mention];
-                        await context.sendActivity(replyActivity);
-                    }
-                    break;
-                }
-                case 'get queue position': {
-                    if (this.activeQueue) {
-                        if (!this.activeQueue.checkQueue(context.activity.from.id)) {
-                            await context.sendActivity('You are currently not in a queue.');
-                        } else {
-                            const mention = {
-                                mentioned: context.activity.from,
-                                text: `<at>${new TextEncoder().encode(context.activity.from.name)}</at>`,
-                                type: 'mention',
-                            };
-                            const replyActivity = MessageFactory.text(
-                                `Hello ${mention.text}! You are currently in position ${
-                                    this.activeQueue.getQueuePosition(context.activity.from.id) + 1
-                                }.`
-                            );
-                            replyActivity.entities = [mention];
-                            await context.sendActivity(replyActivity);
-                        }
-                    } else {
-                        await context.sendActivity(
-                            'Currently no office hours being held. Please check the schedule to confirm the next office hours session!'
-                        );
-                    }
-                    break;
-                }
+                text: `<at>${new TextEncoder().encode(
+                  context.activity.from.name
+                )}</at>`,
+                type: "mention",
+              };
+              const replyActivity = MessageFactory.text(
+                `Hello ${mention.text}! You are currently in position ${
+                  this.activeQueue.getQueuePosition(context.activity.from.id) +
+                  1
+                }.`
+              );
+              replyActivity.entities = [mention];
+              await context.sendActivity(replyActivity);
             }
+          } else {
+            await context.sendActivity(
+              "Currently no office hours being held. Please check the schedule to confirm the next office hours session!"
+            );
+          }
+          break;
+        }
+      }
 
-            // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
-    }
+      // By calling next() you ensure that the next BotHandler is run.
+      await next();
+    });
   }
+}
