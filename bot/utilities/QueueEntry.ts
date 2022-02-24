eec8dc21ee287506b38ff6c4cc111f34f44e30f8
Jack Donkers <jackdonkers@Jacks-MacBook-Pro-2.local>
2022-02-23 17:44:33 -0800

    prettier

--- bot/utilities/QueueEntry.ts
+++ bot/utilities/QueueEntry.ts
@@ -1,48 +1,48 @@
-import { QueueEntryEntity } from '../entities/queueEntry';
+import { QueueEntryEntity } from "../entities/queueEntry";
 
 export default class QueueEntry {
-    id: number;
-    userId: string;
-    queueId: number;
-    privateEntry: boolean;
-    question: string;
-    resolved: boolean;
-    createdAt: Date;
-    updatedAt: Date;
+  id: number;
+  userId: string;
+  queueId: number;
+  privateEntry: boolean;
+  question: string;
+  resolved: boolean;
+  createdAt: Date;
+  updatedAt: Date;
 
-    constructor({ id, userId, queueId, privateEntry, question, resolved }) {
-        this.id = id;
-        this.userId = userId;
-        this.queueId = queueId;
-        this.privateEntry = privateEntry;
-        this.question = question;
-        this.resolved = resolved;
-    }
+  constructor({ id, userId, queueId, privateEntry, question, resolved }) {
+    this.id = id;
+    this.userId = userId;
+    this.queueId = queueId;
+    this.privateEntry = privateEntry;
+    this.question = question;
+    this.resolved = resolved;
+  }
 
-    setQueueId(queueId: number): void {
-        this.queueId = queueId;
-    }
+  setQueueId(queueId: number): void {
+    this.queueId = queueId;
+  }
 
-    static fromQueueEntryEntity(queueEntryEntity: QueueEntryEntity): QueueEntry {
-        const entry = new QueueEntry({
-            id: queueEntryEntity.id,
-            userId: queueEntryEntity.userId,
-            queueId: queueEntryEntity.queue,
-            privateEntry: queueEntryEntity.privateEntry,
-            question: queueEntryEntity.question,
-            resolved: queueEntryEntity.resolved,
-        });
-        entry.createdAt = queueEntryEntity.createdAt;
-        entry.updatedAt = queueEntryEntity.updatedAt;
-        return entry;
-    }
+  static fromQueueEntryEntity(queueEntryEntity: QueueEntryEntity): QueueEntry {
+    const entry = new QueueEntry({
+      id: queueEntryEntity.id,
+      userId: queueEntryEntity.userId,
+      queueId: queueEntryEntity.queue,
+      privateEntry: queueEntryEntity.privateEntry,
+      question: queueEntryEntity.question,
+      resolved: queueEntryEntity.resolved,
+    });
+    entry.createdAt = queueEntryEntity.createdAt;
+    entry.updatedAt = queueEntryEntity.updatedAt;
+    return entry;
+  }
 
-    
-
-    toString(showPrivate: boolean = false): string {
-        return (
-            `       userId: ${this.userId},\n` +
-            `     question: ${!this.privateEntry || showPrivate ? this.question : 'private'},\n`
-        );
-    }
+  toString(showPrivate: boolean = false): string {
+    return (
+      `       userId: ${this.userId},\n` +
+      `     question: ${
+        !this.privateEntry || showPrivate ? this.question : "private"
+      },\n`
+    );
+  }
 }
